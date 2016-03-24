1443127694dc2ed99de5c515231d6262efc40f46
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2016-03-24 19:54:44 -0400

    Style upgrade

--- lib/gfa/graph.rb
+++ lib/gfa/graph.rb
@@ -28,39 +28,47 @@ class GFA
    private
    
       def segment_names_with_orient
-	 segments.map do |s|
-	    %w[+ -].map{ |orient| GFA::GraphVertex.idx(s, orient) }
-	 end.flatten(1).to_set
+         segments.flat_map do |s|
+            %w[+ -].map{ |orient| GFA::GraphVertex.idx(s, orient) }
+         end.to_set
       end
 
       def segment_names
-	 segments.map do |s|
-	    GFA::GraphVertex.idx(s, nil)
-	 end.to_set
+         segments.map do |s|
+            GFA::GraphVertex.idx(s, nil)
+         end.to_set
       end
 
       def rgl_implicit_graph(opts)
+         opts = rgl_implicit_options(opts)
+         RGL::ImplicitGraph.new do |g|
+            g.vertex_iterator do |b|
+               (opts[:orient] ? segment_names_with_orient :
+                  segment_names).each(&b)
+            end
+            g.adjacent_iterator do |x,b|
+               rgl_implicit_adjacent_iterator(x,b,opts)
+            end
+            g.directed = opts[:directed]
+         end
+      end
+
+      def rgl_implicit_options(opts)
          opts[:orient] = true if opts[:orient].nil?
-	 opts[:directed] = opts[:orient] if opts[:directed].nil?
-	 
-	 RGL::ImplicitGraph.new do |g|
-	    g.vertex_iterator do |b|
-	       (opts[:orient] ? segment_names_with_orient :
-		  segment_names).each &b
-	    end
-	    g.adjacent_iterator do |x,b|
-	       links.each do |l|
-		  if l.from?(x.segment, x.orient)
-		     orient = opts[:orient] ? l.to_orient : nil
-		     b.call(GFA::GraphVertex.idx(l.to, orient))
-		  elsif opts[:orient] and l.to?(x.segment, orient_rc(x.orient))
-		     orient = orient_rc(l.from_orient.value)
-		     b.call(GFA::GraphVertex.idx(l.from, orient))
-		  end
-	       end
-	    end
-	    g.directed = opts[:directed]
-	 end
+         opts[:directed] = opts[:orient] if opts[:directed].nil?
+         opts
+      end
+
+      def rgl_implicit_adjacent_iterator(x,b,opts)
+         links.each do |l|
+            if l.from?(x.segment, x.orient)
+               orient = opts[:orient] ? l.to_orient : nil
+               b.call(GFA::GraphVertex.idx(l.to, orient))
+            elsif opts[:orient] and l.to?(x.segment, orient_rc(x.orient))
+               orient = orient_rc(l.from_orient.value)
+               b.call(GFA::GraphVertex.idx(l.from, orient))
+            end
+         end
       end
 
       def orient_rc(o) o=="+" ? "-" : "+" ; end
@@ -83,7 +91,7 @@ class GFA::GraphVertex # :nodoc:
    
    def initialize(segment, orient)
       @segment = segment.is_a?(GFA::Record::Segment) ? segment.name.value :
-		 segment.is_a?(GFA::Field) ? segment.value : segment
+                 segment.is_a?(GFA::Field) ? segment.value : segment
       @orient  = orient.is_a?(GFA::Field) ? orient.value : orient
    end
 
--- lib/gfa/record.rb
+++ lib/gfa/record.rb
@@ -29,14 +29,12 @@ class GFA::Record
    
    def to_s
       o = [code.to_s]
-      i = 2
       REQ_FIELDS.each_index do |i|
-	 o << fields[i].to_s(false)
-	 i += 1
+         o << fields[i].to_s(false)
       end
       fields.each do |k,v|
-	 next if k.is_a? Integer
-	 o << "#{k}:#{v.to_s}"
+         next if k.is_a? Integer
+         o << "#{k}:#{v}"
       end
       o.join("\t")
    end
@@ -49,29 +47,29 @@ class GFA::Record
       
       def add_field(f_tag, f_type, f_value, format=nil)
          unless format.nil?
-	    msg = (f_tag.is_a?(Integer) ? "column #{f_tag}" : "#{f_tag} field")
-	    GFA::assert_format(f_value, format, "Bad #{type} #{msg}")
-	 end
-	 f_type_name = ::GFA::Field::CODES[ f_type ]
-	 if f_type_name.nil?
-	    raise "Unknown field type: #{f_type}."
-	 end
-	 klass = Object.const_get("GFA::Field::#{f_type_name}")
-	 @fields[ f_tag ] = klass.new(f_value)
+            msg = (f_tag.is_a?(Integer) ? "column #{f_tag}" : "#{f_tag} field")
+            GFA::assert_format(f_value, format, "Bad #{type} #{msg}")
+         end
+         f_type_name = ::GFA::Field::CODES[ f_type ]
+         if f_type_name.nil?
+            raise "Unknown field type: #{f_type}."
+         end
+         klass = Object.const_get("GFA::Field::#{f_type_name}")
+         @fields[ f_tag ] = klass.new(f_value)
       end
       
       def add_opt_field(f, known)
-	 m = /^([A-Za-z]+):([A-Za-z]+):(.*)$/.match(f) or
-	    raise "Cannot parse field: '#{f}'."
-	 f_tag = m[1].to_sym
-	 f_type = m[2].to_sym
-	 f_value = m[3]
-	 raise "Unknown reserved tag #{f_tag} for a #{type} record." if
-	    known[f_tag].nil? and f_tag =~ /^[A-Z]+$/
-	 raise "Wrong field type #{f_type} for a #{f_tag} tag," +
-	    " expected #{known[f_tag]}" unless
-	    known[f_tag].nil? or known[f_tag] == f_type
-	 add_field(f_tag, f_type, f_value)
+         m = /^([A-Za-z]+):([A-Za-z]+):(.*)$/.match(f) or
+            raise "Cannot parse field: '#{f}'."
+         f_tag = m[1].to_sym
+         f_type = m[2].to_sym
+         f_value = m[3]
+         raise "Unknown reserved tag #{f_tag} for a #{type} record." if
+            known[f_tag].nil? and f_tag =~ /^[A-Z]+$/
+         raise "Wrong field type #{f_type} for a #{f_tag} tag," +
+            " expected #{known[f_tag]}" unless
+            known[f_tag].nil? or known[f_tag] == f_type
+         add_field(f_tag, f_type, f_value)
       end
 
 end
