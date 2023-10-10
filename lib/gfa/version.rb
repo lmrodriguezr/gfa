dd7e11df47fa9efd9c0b2f6687896c81dbf1b2fb
Luis M. Rodriguez-R <lmrodriguezr@gmail.com>
2023-10-10 15:24:37 +0200

    New option `orphan` in `gfa-paths-to-fasta`

--- lib/gfa/version.rb
+++ lib/gfa/version.rb
@@ -1,5 +1,5 @@
 class GFA
-  VERSION       = '0.6.3'
+  VERSION       = '0.7.0'
   VERSION_ARRAY = VERSION.split(/\./).map { |x| x.to_i } # :nodoc:
   VERSION_MAJOR = VERSION_ARRAY[0] # :nodoc:
   VERSION_MINOR = VERSION_ARRAY[1] # :nodoc:
